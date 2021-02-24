ee23c2a5ab7cccea8a7f08fb0d26def22467685b
Erica Moreli <ericammoreli@gmail.com>
2021-02-24 19:59:37 -0300

    Currency converter - API request configuration / response convert / AppBar

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(MaterialApp(home: Container()));
+const request = "https://api.hgbrasil.com/finance?format=json&key=6cf64cac";
+
+void main() async {
+  runApp(MaterialApp(home: Home()));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Currency Converter \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
