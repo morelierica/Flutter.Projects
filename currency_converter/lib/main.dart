0f8218cb718dcee79e023a6abb3981fbc320b574
Erica Moreli <ericammoreli@gmail.com>
2021-03-01 18:04:54 -0300

    Currency converter - FutureBuilder implementation

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -23,12 +23,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("\$ Currency Converter \$"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
-    );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Currency Converter \$"),
+          backgroundColor: Colors.teal[700],
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Text(
+                      "Loading...",
+                      style: TextStyle(color: Colors.teal[700], fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        "Error loading data",
+                        style:
+                            TextStyle(color: Colors.teal[700], fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+              }
+            }));
   }
 }
