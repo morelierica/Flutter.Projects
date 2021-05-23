c052b8969d14fbfeff314f63aa64e4dcc3abcc8b
Erica Moreli <ericammoreli@gmail.com>
2021-05-23 11:30:00 -0300

    Gifs finder - Layout creation and adding a ProgressIndicator

--- gifs_finder/lib/main.dart
+++ gifs_finder/lib/main.dart
@@ -4,5 +4,15 @@ import 'package:gifs_finder/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
   ));
 }
