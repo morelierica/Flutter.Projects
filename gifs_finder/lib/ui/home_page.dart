7a0c38c57a1b5f5c1510d8f56fed3884c072cfdd
Erica Moreli <ericammoreli@gmail.com>
2021-05-23 11:58:56 -0300

    Gifs finder - Creating gif table

--- gifs_finder/lib/ui/home_page.dart
+++ gifs_finder/lib/ui/home_page.dart
@@ -89,5 +89,20 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {}
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        });
+  }
 }
