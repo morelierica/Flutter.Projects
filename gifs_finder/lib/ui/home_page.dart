fceaf7a6331d9400a8a257ee3c4a1370b4320746
Erica Moreli <ericammoreli@gmail.com>
2021-05-23 10:21:46 -0300

    Gifs finder - Data request implementation

--- gifs_finder/lib/ui/home_page.dart
+++ gifs_finder/lib/ui/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,6 +11,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    _search == null
+        ? response = await http.get(
+            "https://api.giphy.com/v1/gifs/trending?api_key=0wjGKFf8LJWGDHaainPqMNAMCuKoB2or&limit=20&rating=g")
+        : response = await http.get(
+            "https://api.giphy.com/v1/gifs/search?api_key=0wjGKFf8LJWGDHaainPqMNAMCuKoB2or&q=$_search&limit=20&offset=$_offset&rating=g&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map); //checking if get method has results
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
